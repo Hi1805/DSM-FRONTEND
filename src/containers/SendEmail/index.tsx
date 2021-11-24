4bc0d49721f3e75fdd50c0ec536b52c2a31ed375
Ho-Thanh <79647705+Ho-Thanh@users.noreply.github.com>
2021-11-24 21:13:48 +0700

    email

--- src/containers/SendEmail/index.tsx
+++ src/containers/SendEmail/index.tsx
@@ -1,9 +1,49 @@
 import React from 'react'
-
+import Style from './style'
 export const SendEmail = () => {
     return (
-        <div>
+        <Style>
+            <div className="layout__content">
+      
+                <form className="layout__content__form">
+                    <div className="form__title">
+                        <h2>From :  Admin</h2>
+                        <h4>Send email to all</h4>
+                        <select className="form__title__select form-select">
+                            <option value={"teachers"} selected> Teachers </option>
+                            <option value={"students"}>Students</option>
+                            
+                        </select> 
+                    </div>
+                    <div className="form__body">
+                        <div className="form-group form__body__title">
+                            <label htmlFor="title_email">Title</label>
+                            <input type="text" className="form-control" id="title_email" placeholder="title" />
+                        </div>
+                        <div className="form-group content">
+                            <label htmlFor="content_email">Content</label>
+                            
+                            <textarea name="content_email" id="input_content_email" className="form-control" rows={5} placeholder="type content..." />
+                        </div>
+                        <div className="upload">
+                            <button type="submit" className="upload__button">Upload file
+                            <i className="fas fa-upload"></i>
+                            </button>
+                        </div>
+                        <div className="show-file">
 
-        </div>
+                        </div>
+                    
+                        <div className="form__body__buttonSend">
+                            <button type="button" className="btn btn-success send">Send</button>
+                        </div>
+                    
+                    </div>
+                    
+                </form>
+
+            
+            </div>
+        </Style>
     )
 }
--- src/screens/SendEmailScreen/index.tsx
+++ src/screens/SendEmailScreen/index.tsx
@@ -1,6 +1,18 @@
 import React from "react";
 import { LayoutManage } from "template/LayoutManage";
+import {  Navbar } from "containers";
+
+import { SendEmail } from "containers";
 
 export const SendEmailScreen = () => {
-  return <LayoutManage></LayoutManage>;
+  
+  return (
+    
+  <LayoutManage>
+    <Navbar
+      title="Send Email"
+      valueSearch=""
+      handleSearch={()=>{}}/>
+    <SendEmail/>
+  </LayoutManage>);
 };
