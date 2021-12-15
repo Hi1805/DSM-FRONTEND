99d00005768db3d65cdfc19112459cbb4aee8504
Huy Trương Thành <ripker1805@gmail.com>
2021-12-15 18:36:30 +0700

    fix ngu 2

--- src/modules/students/reducers.ts
+++ src/modules/students/reducers.ts
@@ -1,14 +1,15 @@
 import { StudentListActions } from "./actions";
 import { STUDENT_FETCH_LIST, STUDENT_LIST_DATA } from "./constants";
 import { ListStudentState } from "./types";
+import { PAGE, SIZE } from "constants/constants";
 
 export const initListStudent: ListStudentState = {
   payload: {
     list: [],
     total: 0,
     pagination: {
-      size: 1,
-      page: 8,
+      size: SIZE,
+      page: PAGE,
       isSort: true,
     },
   },
